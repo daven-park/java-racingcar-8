637667936cf088854ed3d9e32f99258c69d82aa4
ChangHyun Park <buschanny@gmail.com>
2025-10-27 22:26:53 +0900

    feat : impl OutputView requirements

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,25 @@
+package racingcar.view;
+
+import racingcar.domain.Car;
+
+import java.util.List;
+
+public class OutputView {
+
+    private static final String WINNER_RESULT_OUTPUT = "최종 우승자 : ";
+    private static final String RACE_PROGRESS_BAR = "-";
+
+    public void printRound(List<Car> carList) {
+        for (Car car : carList){
+            String carMoveCountProgress = RACE_PROGRESS_BAR.repeat(car.getCarMoveCount());
+            System.out.println(car.getCarName() + " : " + carMoveCountProgress);
+        }
+        System.out.println();
+    }
+
+    public void printWinnerList(List<String> winnerList){
+        String winnerNameList = String.join(", ", winnerList);
+        System.out.println(WINNER_RESULT_OUTPUT + winnerNameList);
+    }
+
+}
