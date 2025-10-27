20c1ee75d1447cd0baf0736333cae9d279f9e39b
ChangHyun Park <buschanny@gmail.com>
2025-10-27 22:21:46 +0900

    feat : impl Car Object Class

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,28 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private final String carName;
+    private int carMoveCount;
+
+    public Car(String name){
+        this.carName = name;
+        this.carMoveCount = 0;
+    }
+
+    public void move(){
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        if (randomNumber >= 4){
+        this.carMoveCount++;
+        }
+    }
+
+    public String getCarName(){
+        return carName;
+    }
+    public int getCarCount(){
+        return carMoveCount;
+    }
+
+}
